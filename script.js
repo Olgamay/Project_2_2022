f57cb3377b2d43abc95ff01a235fbcf3c4e67412
Olgamay <olga48915042@yandex.ru>
2021-05-08 10:07:15 +0300

    от8.05.2021

---
+++ script.js
@@ -0,0 +1,11 @@
+let textInput = document.querySelector('.input');
+let button = document.querySelector('.plus_input');
+button.addEventListener('click', () => {
+    let newElement = document.createElement('p');
+    parentElement.append(newElement);
+    // newText.innerText = textInput.value;
+    // let result = document.querySelector('body');
+    // resultText.appendChild(newText);
+  });
+
+
