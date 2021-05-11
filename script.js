c6be5c5950ea99e5cf99ef2da101af912250e216
Olgamay <olga48915042@yandex.ru>
2021-05-11 23:05:40 +0300

    доработка от 11.05

--- script.js
+++ script.js
@@ -1,11 +1,50 @@
-let textInput = document.querySelector('.input');
-let button = document.querySelector('.plus_input');
-button.addEventListener('click', () => {
-    let newElement = document.createElement('p');
-    parentElement.append(newElement);
-    // newText.innerText = textInput.value;
-    // let result = document.querySelector('body');
-    // resultText.appendChild(newText);
-  });
+let nameDelo = document.querySelector('.name');
+let button = document.querySelector('.plus');
+let todo = document.querySelector('todo');
+// переменная в которую сохранять значение
+let todoList = [];
+// указываем что введенное значение будет являться массивом
+
+// Добавляем обработчик событий
+button.addEventListener('click', function() {
+  let newnameDelo = nameDelo.value;
+  todoList.push(newnameDelo);
+  displayDelos();
+
+});
+
+// вводим функцию, которая будет перебирать массив todoList
+function displayDelos() {
+  let displayDelos = '';
+  todoList.forEach(function(item, i) {
+    displayDelos += item;
+  })
+}
 
 
+
+
+
+
+// document.getElementById('.dobav_2').click => () {
+  
+//   let surnameValue = getElementById('in').value;
+//   let temp = {};
+//     temp.todo = surnameValue;
+//   var i = todoList.length;
+//   todoList[i] = temp;
+//   console.log('todoList');
+//   };
+  // let newElement = document.createElement('li');
+  // newElement.innerText = nameField;
+  // let result = document.querySelector('.input');
+  // result.Element.appendChild(newElement);
+  // const buttonDelete = document.querySelectorAll('.plus_input');
+  // buttonDelete.forEach((item) => {
+  
+  // value=это значение введенное в поле задач;
+  // записываем значение введенное в поле пользователем;
+  // console.log(surnameValue);
+// });
+
+// 
