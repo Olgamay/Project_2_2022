4993d01aa12a40fcaa02ddf96e723dead9c921cd
Olgamay <olga48915042@yandex.ru>
2021-05-13 21:23:52 +0300

    удаление из списка дел

--- script.js
+++ script.js
@@ -18,14 +18,16 @@ button.addEventListener('click', function() {
 
 // вводим функцию, которая будет перебирать массив todoList
 function displayTasks(todoList) {
-    // 1. Если массив пустой, то сделать return
+    // 1. Очистить область для вставки
+  let resultElement = document.querySelector('.wrapper');
+   resultElement.innerHTML = '';
+     // 1. Если массив пустой, то сделать return
     if (todoList.length === 0) {
       return;
     };
     
-  // 1. Очистить область для вставки
-  let resultElement = document.querySelector('.wrapper');
-  resultElement.innerHTML = '';
+  
+ 
 
     todoList.forEach(function(value, index) {
       // 1. Создать элемент li 
@@ -36,60 +38,17 @@ function displayTasks(todoList) {
       resultElement.append(newElement);
       // 2.1 Добавить кнопку удаления
 
-      let newButton = document.createElement('button');
+      let newButton = document.createElement('img');
           // 2.1.1 Создать элемент крестик (удаление)
-          newButton.innerText =  del;
+          newButton.classList.add("crest_input");
+          newButton.setAttribute("src", "images/crestwhite.svg")
           // 2.1.2 Вставить крестик внутрь newElement
-          newButton.appendTo(newElement);
-          
-    });
-        // 2.1.3 Добавить обработчик события на click для созданного крестика 
-
-        del.addEventListener('click', function() {
-          let taskdel = del;
-          alert(taskdel);
-        //   
-        //   if(taskName !== '') {
-        //     todoList.push(taskName);
-        //     displayTasks(todoList);
-        //   };
-        // });
+          newElement.append(newButton);
+          newButton.addEventListener('click', function() {
+            todoList =todoList.filter((item) => {
+              return item !== newElement.innerText
+            });
+            displayTasks(todoList);
+        }); 
     });
   }
-
-  
-// todoList = [
-//   'Купить квартиру',
-//   'Почистить ботинки',
-//   'Посмотреть телевизор'
-// ] 
-
-  
-
-
-
-
-
-
-// document.getElementById('.dobav_2').click => () {
-  
-//   let surnameValue = getElementById('in').value;
-//   let temp = {};
-//     temp.todo = surnameValue;
-//   var i = todoList.length;
-//   todoList[i] = temp;
-//   console.log('todoList');
-//   };
-  // let newElement = document.createElement('li');
-  // newElement.innerText = nameField;
-  // let result = document.querySelector('.input');
-  // result.Element.appendChild(newElement);
-  // const buttonDelete = document.querySelectorAll('.plus_input');
-  // buttonDelete.forEach((item) => {
-  
-  // value=это значение введенное в поле задач;
-  // записываем значение введенное в поле пользователем;
-  // console.log(surnameValue);
-// });
-
-// 
