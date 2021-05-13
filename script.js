7a5e50994a6de940be38783ab85409f61c6fd206
Olgamay <olga48915042@yandex.ru>
2021-05-13 19:16:52 +0300

    13.05.2021

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let nameDelo = document.querySelector('.name');
+let newTaskName = document.querySelector('.name');
 let button = document.querySelector('.dobav');
 let todo = document.querySelector('todo');
 // переменная в которую сохранять значение
@@ -8,27 +8,39 @@ let todoList = [];
 // Добавляем обработчик событий
 button.addEventListener('click', function() {
   let taskName = newTaskName.value;
-
   // 1. Проверить на пустоту впианного в поле значения
+  if(taskName !== null) {
   todoList.push(taskName);
   displayTasks(todoList);
-
+  };
 });
 
 // вводим функцию, которая будет перебирать массив todoList
 function displayTasks(todoList) {
   // 1. Если массив пустой, то сделать return
-
+  if (taskName === null) {
+    let result = "Вы ввели пустое значение";
+    return result;
+  };
+    
   todoList.forEach(function(item, i) {
-    // 1. Создать элемент li  
+    // 1. Создать элемент li 
+    let newElement = document.createElement('div');
 
     // 2. Вставить в него тест из массива, который мы перебираем 
+    newElement.innerText = 'taskName';
+    // 3. Добавить внутрь элемента li крестик для дальнейшего удаления
 
-    // 3. Добавить внутрь элемента li крестик для дальнейшего удаления 
-
-    // 4. вывести элемент внутрь списка .todo 
-  })
+    // 4. вывести элемент внутрь списка .todo
+    let resultElement = document.querySelector('.wrapper');
+    resultElement.appendChild(newElement);
+  });
 }
+  
+  
+  
+
+  
 
 
 
