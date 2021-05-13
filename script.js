3dbd031097c61332ce912c6ddeecea691f1e181a
Olgamay <olga48915042@yandex.ru>
2021-05-13 20:44:12 +0300

    13.05_2

--- script.js
+++ script.js
@@ -1,50 +1,62 @@
 let newTaskName = document.querySelector('.name');
 let button = document.querySelector('.dobav');
 let todo = document.querySelector('todo');
+let del = document.querySelector('.crest_input');
 // переменная в которую сохранять значение
 let todoList = [];
 // указываем что введенное значение будет являться массивом
 
 // Добавляем обработчик событий
 button.addEventListener('click', function() {
-  let taskName = newTaskName.value;
-  // 1. Проверить на пустоту впианного в поле значения
-  if(taskName !== '') {
-    todoList.push(taskName);
-    displayTasks(todoList);
-  };
+    let taskName = newTaskName.value;
+    // 1. Проверить на пустоту впианного в поле значения
+      if(taskName !== '') {
+        todoList.push(taskName);
+        displayTasks(todoList);
+      };
 });
 
 // вводим функцию, которая будет перебирать массив todoList
 function displayTasks(todoList) {
-  // 1. Если массив пустой, то сделать return
-  if (todoList.length === 0) {
-    return;
-  };
+    // 1. Если массив пустой, то сделать return
+    if (todoList.length === 0) {
+      return;
+    };
     
   // 1. Очистить область для вставки
   let resultElement = document.querySelector('.wrapper');
   resultElement.innerHTML = '';
 
-  todoList.forEach(function(value, index) {
-    // 1. Создать элемент li 
-    let newElement = document.createElement('div');
+    todoList.forEach(function(value, index) {
+      // 1. Создать элемент li 
+      let newElement = document.createElement('div');
 
-    // 2. Вставить в него тест из массива, который мы перебираем 
-    newElement.innerText = value;
+      // 2. Вставить в него тест из массива, который мы перебираем 
+      newElement.innerText = value;
+      resultElement.append(newElement);
+      // 2.1 Добавить кнопку удаления
 
-    // 2.1 Добавить кнопку удаления
-        // 2.1.1 Создать элемент крестик (удаление)
+      let newButton = document.createElement('button');
+          // 2.1.1 Создать элемент крестик (удаление)
+          newButton.innerText =  del;
+          // 2.1.2 Вставить крестик внутрь newElement
+          newButton.appendTo(newElement);
+          
+    });
+        // 2.1.3 Добавить обработчик события на click для созданного крестика 
 
-        // 2.1.2 Вставить крестик внутрь newElement
+        del.addEventListener('click', function() {
+          let taskdel = del;
+          alert(taskdel);
+        //   
+        //   if(taskName !== '') {
+        //     todoList.push(taskName);
+        //     displayTasks(todoList);
+        //   };
+        // });
+    });
+  }
 
-        // 2.1.3 Добавить обработчик события на click для созданного крестика
-
-    // 2. вывести элемент внутрь списка .todo
-    resultElement.append(newElement);
-  });
-}
-  
   
 // todoList = [
 //   'Купить квартиру',
